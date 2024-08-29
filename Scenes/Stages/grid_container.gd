547c593479816ee63a26c8fcc568989c95d7f7d0
VM Marco <marcoxativa@gmail.com>
2024-08-29 11:55:48 +0200

    control customization complete + keyboard support for select screen

--- Scenes/Stages/grid_container.gd
+++ Scenes/Stages/grid_container.gd
@@ -9,11 +9,17 @@ class_name CharacterSelect
 @onready var banner_texture_rect : TextureRect = $TextureRect
 
 
-@onready var texture_rect = $TextureRect
-@onready var positionn = $Position
-@onready var position_down = $PositionDown
+@onready var texture_rect := $TextureRect
+@onready var positionn := $Position
+@onready var position_down := $PositionDown
 
 @export var current_map : PackedScene 
+
+@onready var p1_control_selection: ScrollMenu = $ControlSelection/P1_control_selection
+@onready var p2_control_selection: ScrollMenu = $ControlSelection2/P2_control_selection
+
+
+var input_methods : Array[INPUT_METHOD]
 
 enum match_mode {
 	
@@ -55,11 +61,15 @@ func _ready():
 	real_target_position = positionn.position
 	
 	mode = GameManager.character_selection_mode
+	input_methods = [2,2]
+	remap_controllers()
+	Controls.changed_controllers.connect(remap_controllers)
 	
 	await GameManager.online_setup
 	
 	reset_indexes()
 	GDSync.call_func(reset_indexes)
+	
 
 func reset_indexes():
 	selected_index = 0; selected_index2 = 0;
@@ -74,12 +84,12 @@ func _process(delta: float) -> void:
 	choose_cooldown += delta
 	choose_cooldown2 += delta
 	
-	if(choose_cooldown >= .3):
+	if(choose_cooldown >= .3) and not p1_control_selection._is_active:
 		#Player 1
 		if not GameManager.online or GameManager.is_host:
 			input_movement(0)
 			
-	if(choose_cooldown2 >= .3):
+	if(choose_cooldown2 >= .3) and not p2_control_selection._is_active:
 		#Player 2
 		if not GameManager.online and mode == match_mode.LOCAL_2P:
 			input_movement(1)
@@ -92,28 +102,28 @@ func _process(delta: float) -> void:
 
 func input_movement(character_id : int, second_onlineP : bool = false):
 	if ((character_id == 0 and selected_fighter == "") or (character_id == 0 and selected_fighter2 == "" and second_onlineP)) or (character_id == 1 and selected_fighter2 == ""):
-		if Input.is_joy_button_pressed(character_id, Controls.ui["move_up"][0]) or Input.get_joy_axis(character_id, JOY_AXIS_LEFT_Y) < -0.5:
+		if is_joy_button_just_pressed("move_up", input_methods[character_id]) or( Input.get_joy_axis(character_id, JOY_AXIS_LEFT_Y) < -0.5 and input_methods[character_id] != INPUT_METHOD.KEYBOARD):
 			$SelectCharacter.play()
 			move_selection(-grid_width,character_id, second_onlineP) 
 			GDSync.call_func(move_selection,[-grid_width,character_id,second_onlineP])
-		elif Input.is_joy_button_pressed(character_id, Controls.ui["move_down"][0]) or Input.get_joy_axis(character_id, JOY_AXIS_LEFT_Y) > 0.5:
+		elif is_joy_button_just_pressed("move_down", input_methods[character_id]) or (Input.get_joy_axis(character_id, JOY_AXIS_LEFT_Y) > 0.5 and input_methods[character_id] != INPUT_METHOD.KEYBOARD):
 			$SelectCharacter.play()
 			move_selection(grid_width,character_id,second_onlineP)  
 			GDSync.call_func(move_selection,[grid_width,character_id,second_onlineP])
-		elif Input.is_joy_button_pressed(character_id, Controls.ui["move_left"][0]) or Input.get_joy_axis(character_id, JOY_AXIS_LEFT_X) < -0.5:
+		elif is_joy_button_just_pressed("move_left", input_methods[character_id]) or (Input.get_joy_axis(character_id, JOY_AXIS_LEFT_X) < -0.5 and input_methods[character_id] != INPUT_METHOD.KEYBOARD):
 			$SelectCharacter.play()
 			move_selection(-1,character_id,second_onlineP) 
 			GDSync.call_func(move_selection,[-1,character_id,second_onlineP],)
-		elif Input.is_joy_button_pressed(character_id, Controls.ui["move_right"][0]) or Input.get_joy_axis(character_id, JOY_AXIS_LEFT_X) > 0.5:
+		elif is_joy_button_just_pressed("move_right", input_methods[character_id]) or (Input.get_joy_axis(character_id, JOY_AXIS_LEFT_X) > 0.5 and input_methods[character_id] != INPUT_METHOD.KEYBOARD):
 			$SelectCharacter.play()
 			move_selection(1,character_id,second_onlineP) 
 			GDSync.call_func(move_selection,[1,character_id,second_onlineP])
-		elif is_joy_button_just_pressed("accept",character_id):
+		elif is_joy_button_just_pressed("accept", input_methods[character_id]):
 			$MenuSelect.play()
 			_select_fighter(character_id, second_onlineP)
 			GDSync.call_func(_select_fighter,[character_id,second_onlineP])
 	else:
-		if Input.is_joy_button_pressed(character_id, Controls.ui["go_back"][0]):
+		if is_joy_button_just_pressed("go_back", input_methods[character_id]):
 			$MenuSelect.play()
 			if mode == match_mode.CPU:
 				if selected_fighter2 != "": selected_fighter2 = ""
@@ -230,10 +240,48 @@ var action_state : Dictionary = {
 	"go_back" : false
 }
 
-func is_joy_button_just_pressed(action_name : String, device : int = 0):
-	if action_state[action_name] == false and Input.is_joy_button_pressed(device, Controls.ui[action_name][0]):
-		action_state[action_name] = true
-		return true
-	if not Input.is_joy_button_pressed(device, Controls.ui[action_name][0]):
-		action_state[action_name] = false
-	return false
+enum INPUT_METHOD{
+	CONTROLLER_1,
+	CONTROLLER_2,
+	KEYBOARD
+}
+
+func is_joy_button_just_pressed(action_name : String, input_method : int = 0) -> bool:
+	if input_method != INPUT_METHOD.KEYBOARD:
+		if action_state[action_name] == false and Input.is_joy_button_pressed( input_method, Controls.ui[action_name][0]) :
+			action_state[action_name] = true
+			return true
+		if not Input.is_joy_button_pressed(input_method, Controls.ui[action_name][0]):
+			action_state[action_name] = false
+		return false
+	else:
+		if action_state[action_name] == false and Input.is_physical_key_pressed(Controls.ui[action_name][1]) :
+			action_state[action_name] = true
+			return true
+		if not Input.is_physical_key_pressed(Controls.ui[action_name][1]):
+			action_state[action_name] = false
+		return false
+
+
+func is_mapped_action_pressed(action_name : String, input_method : int = 0) -> bool:
+	if input_method != INPUT_METHOD.KEYBOARD:
+		if Input.is_joy_button_pressed( input_method, Controls.ui[action_name][0]):
+			return true
+		else:
+			return false
+			
+	else:
+		if Input.is_physical_key_pressed(Controls.ui[action_name][1]):
+			return true
+		else:
+			return false
+
+
+func remap_controllers():
+	match Controls.connected_controllers:
+		0: 
+			input_methods[0] = 2 #Player1 uses keyboard
+		1: 
+			input_methods[0] = 0; input_methods[1] = 2; #Player1 uses controller and p2 uses keyboard
+		2: 
+			input_methods[0] = 0; input_methods[1] = 1; #Player1 uses controller and p2 uses controller
