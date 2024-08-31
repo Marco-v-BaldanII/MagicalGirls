8ae36e31fc3c87a1513f6ad3e3ddb5f17011aff9
VM Marco <marcoxativa@gmail.com>
2024-08-31 14:00:46 +0200

    Larissa drum

--- Scenes/Entities/larissa.gd
+++ Scenes/Entities/larissa.gd
@@ -1,2 +1,21 @@
 extends Player
 class_name Larissa
+
+func set_hitboxes(player_id : int):
+	if player_num == 1:
+		hit_box_1.set_collision_layer_value(2, true)
+		hit_box_2.set_collision_layer_value(2, true)
+		$hit_boxes/special_box.set_collision_layer_value(2, true)
+		hurt_box.set_collision_mask_value(3, true)
+		hurt_box.set_collision_layer_value(4,true)
+		head_hurt_box.set_collision_mask_value(3,true)
+		hurt_box_layer = 4
+
+	else:
+		hit_box_1.set_collision_layer_value(3, true)
+		hit_box_2.set_collision_layer_value(3, true)
+		$hit_boxes/special_box.set_collision_layer_value(3, true)
+		hurt_box.set_collision_mask_value(2, true)
+		hurt_box.set_collision_layer_value(5,true)
+		head_hurt_box.set_collision_mask_value(2,true)
+		hurt_box_layer = 5
