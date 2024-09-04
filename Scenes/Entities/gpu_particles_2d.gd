37542b7cc57668a0f5a3be4b40738486bf950df7
VM Marco <marcoxativa@gmail.com>
2024-09-04 20:21:03 +0200

    JAJAJAJAJA

--- Scenes/Entities/gpu_particles_2d.gd
+++ Scenes/Entities/gpu_particles_2d.gd
@@ -16,11 +16,8 @@ func _process(delta):
 
 	# Get the current frame index (linear index assuming the frames are numbered row-major)
 	var current_frame = $"..".frame
-	print("current frame"+ str(current_frame))
 	var total_frames = hframes * vframes
 	var frame_index = current_frame % total_frames
 
 	# Pass the frame index directly to the shader
 	process_material.set_shader_parameter("tex_anim_offset", float(frame_index)/108.0)
-	
-	print(frame_index/108.0)
