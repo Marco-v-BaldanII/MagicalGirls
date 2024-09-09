8f6356acd4c73564407b5a7c4534fa54fc9a3f02
VM Marco <marcoxativa@gmail.com>
2024-09-09 14:47:18 +0200

    vcx

--- Scenes/Entities/Ritsu.gd
+++ Scenes/Entities/Ritsu.gd
@@ -56,6 +56,7 @@ func perform_move():
 						if specials.contains("ulti"):
 							sprite_2d.modulate = Color(0,0.39,0.95,1)
 							match_setting.inverted_effect(global_position, direction)
+							if GameManager.online: GDSync.call_func(match_setting.inverted_effect, [global_position, direction])
 							
 							var special_scene : PackedScene = load("res://Scenes/projectiles/"+specials+".tscn")
 
