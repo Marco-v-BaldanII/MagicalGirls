bd5caccd3ba19e0247b9fb7222908aad22fdfdb5
paolo2kk <gpaucayuela@gmail.com>
2024-09-13 13:03:36 +0200

    awdfwd

---
+++ Cutscenes/Cutscene4.gd
@@ -0,0 +1,19 @@
+extends Node2D
+@onready var timer = $Timer
+var finished: bool = false
+var finish_cutscene: Signal
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.start()
+	pass # Replace with function body.
+
+
+
+
+func _on_timer_timeout():
+	finish_cutscene.emit()
+	finished = true
+	print(finished)
+	pass # Replace with function body.
